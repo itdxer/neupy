Update function tests. Fix hinton diagram for only positive values in input matrix. Update softmax derivative function.

neupy/plots/hinton.py
@@ -58,7 +58,7 @@ def hinton(matrix, max_weight=None, ax=None, add_legend=True):
     ax.xaxis.set_major_locator(plt.NullLocator())
     ax.yaxis.set_major_locator(plt.NullLocator())
 
-    for (x, y), weight in np.ndenumerate(matrix):
+    for (y, x), weight in np.ndenumerate(matrix):
         color = 'white' if weight > 0 else 'black'
         size = min(np.sqrt(np.abs(weight / max_weight)), 1)
         rect = plt.Rectangle([x - size / 2, y - size / 2], size, size,
@@ -69,18 +69,30 @@ def hinton(matrix, max_weight=None, ax=None, add_legend=True):
     ax.invert_yaxis()
 
     if add_legend:
-        # Define a legend for the plot
+        max_value = matrix.max().round(2)
+        min_value = matrix.min().round(2)
+
         white = Rectangle((0, 0), 1, 1, linewidth=1, linestyle='solid',
                           facecolor='#ffffff')
-        black = Rectangle((0, 0), 1, 1, color='#000000')
-        ax.legend(
-            [white, black],
-            [
-                'Positive value\nMax: {}'.format(matrix.max().round(2)),
-                'Negative value\nMin: {}'.format(matrix.min().round(2))
-            ],
-            loc='center left',
-            bbox_to_anchor=(1, 0.5)
-        )
+        rectangles = [white]
+
+        if min_value < 0:
+            black = Rectangle((0, 0), 1, 1, color='#000000')
+            rectangles.append(black)
+            rect_description = [
+                'Positive value\n'
+                'Max: {}'.format(max_value),
+                'Negative value\n'
+                'Min: {}'.format(min_value),
+            ]
+        else:
+            rect_description = [
+                'Positive value\n'
+                'Max: {}\n'
+                'Min: {}'.format(max_value, min_value),
+            ]
+
+        ax.legend(rectangles, rect_description, loc='center left',
+                  bbox_to_anchor=(1, 0.5))
 
     return ax

tests/core/test_functions.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import numpy as np
 
 from neupy.functions import with_derivative
@@ -5,6 +7,11 @@ from neupy.functions import *
 from neupy.functions import *
 
 from base import BaseTestCase
+
+
+def differentiate(func, args, epsilon=1e-5):
+    args = np.array(args)
+    return (func(args + epsilon) - func(args - epsilon)) / (2 * epsilon)
 
 
 class FunctionTestCase(BaseTestCase):
@@ -56,44 +63,242 @@ class FunctionTestCase(BaseTestCase):
         self.assertEqual(step(0), 0)
         self.assertEqual(step(1), 1)
 
+        test_matrix = np.array([
+            [-1, -100, 0.0001, -0.0001],
+            [0, 100, 1, 1.0001],
+        ])
+        np.testing.assert_array_almost_equal(
+            step(test_matrix),
+            np.array([
+                [0, 0, 1, 0],
+                [0, 1, 1, 1],
+            ])
+        )
+
     def test_linear(self):
         self.assertEqual(linear(-1), -1)
         self.assertEqual(linear.deriv(100), 1)
 
-    def test_sigmoid(self):
-        self.assertEqual(round(sigmoid(0.7), 3), 0.668)
-        self.assertEqual(round(sigmoid(0.7, alpha=-0.1), 3), 0.483)
-        self.assertEqual(round(sigmoid.deriv(0.7), 3), 0.222)
-        self.assertEqual(round(sigmoid.deriv(0.7, alpha=-0.1), 3), -0.025)
-        self.assertEqual(round(sigmoid.deriv.deriv(0.7), 3), -0.075)
-        self.assertEqual(
-            round(sigmoid.deriv.deriv(0.7, alpha=2), 3), -0.384
+        test_matrix = np.random.random((4, 5))
+        np.testing.assert_array_almost_equal(
+            linear(test_matrix),
+            test_matrix
+        )
+        np.testing.assert_array_almost_equal(
+            linear.deriv(test_matrix),
+            np.ones(test_matrix.shape)
         )
 
-    def test_tanh(self):
-        # tanh
-        self.assertEqual(round(tanh(0.7), 3), 0.604)
-        self.assertEqual(round(tanh(0.7, 0.5), 3), 0.336)
-        self.assertEqual(round(tanh.deriv(0.7, 0.1), 3), 0.1)
-        self.assertEqual(round(tanh.deriv.deriv(0.7, 0.8), 3), -0.482)
+    def test_sigmoid_scalars(self):
+        self.assertAlmostEqual(sigmoid(0.7), 0.668, places=3)
+        self.assertAlmostEqual(sigmoid(0.7, alpha=-0.1), 0.483, places=3)
+
+        self.assertAlmostEqual(
+            sigmoid.deriv(0.7),
+            differentiate(sigmoid, 0.7)
+        )
+        self.assertAlmostEqual(
+            sigmoid.deriv(0.7, alpha=-0.1),
+            differentiate(partial(sigmoid, alpha=-0.1), 0.7)
+        )
+        self.assertAlmostEqual(
+            sigmoid.deriv.deriv(0.7),
+            differentiate(sigmoid.deriv, 0.7)
+        )
+        self.assertAlmostEqual(
+            sigmoid.deriv.deriv(0.7, alpha=2),
+            differentiate(partial(sigmoid.deriv, alpha=2), 0.7)
+        )
+
+    def test_sigmoid_vectors(self):
+        test_vector = np.array([1, 2, 4, -0.33])
+
+        np.testing.assert_array_almost_equal(
+            sigmoid(test_vector),
+            np.array([0.731,  0.881,  0.982,  0.418]),
+            decimal=3
+        )
+        np.testing.assert_array_almost_equal(
+            sigmoid.deriv.deriv(test_vector, alpha=2),
+            differentiate(partial(sigmoid.deriv, alpha=2), test_vector)
+        )
+        np.testing.assert_array_almost_equal(
+            sigmoid.deriv(test_vector, alpha=2),
+            differentiate(partial(sigmoid, alpha=2), test_vector)
+        )
+
+    def test_sigmoid_matrix(self):
+        test_matrix = np.array([
+            [1, 2, 4, -0.33],
+            [-10, 0, 10, 1.45],
+        ])
+
+        np.testing.assert_array_almost_equal(
+            sigmoid(test_matrix),
+            np.array([
+                [0.731, 0.881, 0.982, 0.418],
+                [0, 0.5, 1, 0.81],
+            ]),
+            decimal=3
+        )
+        np.testing.assert_array_almost_equal(
+            sigmoid.deriv.deriv(test_matrix, alpha=2),
+            differentiate(partial(sigmoid.deriv, alpha=2), test_matrix)
+        )
+        np.testing.assert_array_almost_equal(
+            sigmoid.deriv(test_matrix, alpha=2),
+            differentiate(partial(sigmoid, alpha=2), test_matrix)
+        )
+
+    def test_tanh_scalar(self):
+        self.assertAlmostEqual(tanh(0.7), 0.604, places=3)
+        self.assertAlmostEqual(tanh(0.7, 0.5), 0.336, places=3)
+
+        self.assertAlmostEqual(
+            tanh.deriv(0.7),
+            differentiate(tanh, 0.7)
+        )
+        self.assertAlmostEqual(
+            tanh.deriv(0.7, alpha=-0.1),
+            differentiate(partial(tanh, alpha=-0.1), 0.7)
+        )
+        self.assertAlmostEqual(
+            tanh.deriv.deriv(0.7),
+            differentiate(tanh.deriv, 0.7)
+        )
+        self.assertAlmostEqual(
+            tanh.deriv.deriv(0.7, alpha=2),
+            differentiate(partial(tanh.deriv, alpha=2), 0.7)
+        )
+
+    def test_tanh_vectors(self):
+        test_vector = np.array([1, 2, 4, -0.33])
+
+        np.testing.assert_array_almost_equal(
+            tanh(test_vector),
+            np.array([0.762, 0.964, 0.999, -0.319]),
+            decimal=3
+        )
+        np.testing.assert_array_almost_equal(
+            tanh.deriv.deriv(test_vector, alpha=2),
+            differentiate(partial(tanh.deriv, alpha=2), test_vector)
+        )
+        np.testing.assert_array_almost_equal(
+            tanh.deriv(test_vector, alpha=2),
+            differentiate(partial(tanh, alpha=2), test_vector)
+        )
+
+    def test_tanh_matrix(self):
+        test_matrix = np.array([
+            [1, 2, 4, -0.33],
+            [-10, 0, 10, 1.45],
+        ])
+
+        np.testing.assert_array_almost_equal(
+            tanh(test_matrix),
+            np.array([
+                [0.762, 0.964, 0.999, -0.319],
+                [-1, 0, 1, 0.896],
+            ]),
+            decimal=3
+        )
+        np.testing.assert_array_almost_equal(
+            tanh.deriv.deriv(test_matrix, alpha=2),
+            differentiate(partial(tanh.deriv, alpha=2), test_matrix)
+        )
+        np.testing.assert_array_almost_equal(
+            tanh.deriv(test_matrix, alpha=2),
+            differentiate(partial(tanh, alpha=2), test_matrix)
+        )
 
     def test_rectifier(self):
-        # rectifier
         self.assertEqual(rectifier(1), 1)
         self.assertEqual(rectifier(0), 0)
         self.assertEqual(rectifier(-1), 0)
 
-    def test_softplus(self):
-        # softplus
-        self.assertEqual(round(softplus(1), 5), 1.31326)
-        self.assertEqual(round(softplus(0), 5), 0.69315)
-        self.assertEqual(round(softplus(-2), 5), 0.12693)
+        np.testing.assert_array_almost_equal(
+            rectifier(
+                np.array([
+                    [-10, -0.33, 0, -1],
+                    [10, 0, 1, 0.33],
+                ])
+            ),
+            np.array([
+                [0, 0, 0, 0],
+                [10, 0, 1, 0.33],
+            ])
+        )
+
+    def test_softplus_scalar(self):
+        self.assertAlmostEqual(softplus(1), 1.313, places=3)
+        self.assertAlmostEqual(softplus(0), 0.693, places=3)
+        self.assertAlmostEqual(softplus(-2), 0.127, places=3)
+
+        self.assertAlmostEqual(
+            softplus.deriv(0.7),
+            differentiate(softplus, 0.7)
+        )
+        self.assertAlmostEqual(
+            softplus.deriv.deriv(0.7),
+            differentiate(softplus.deriv, 0.7)
+        )
+
+    def test_softplus_vectors(self):
+        test_vector = np.array([1, 2, 4, -0.33])
+
+        np.testing.assert_array_almost_equal(
+            softplus(test_vector),
+            np.array([1.313, 2.127, 4.018, 0.542]),
+            decimal=3
+        )
+        np.testing.assert_array_almost_equal(
+            softplus.deriv.deriv(test_vector),
+            differentiate(softplus.deriv, test_vector)
+        )
+        np.testing.assert_array_almost_equal(
+            softplus.deriv(test_vector),
+            differentiate(softplus, test_vector)
+        )
+
+    def test_softplus_matrix(self):
+        test_matrix = np.array([
+            [1, 2, 4, -0.33],
+            [-10, 0, 10, 1.45],
+        ])
+
+        np.testing.assert_array_almost_equal(
+            softplus(test_matrix),
+            np.array([
+                [1.313, 2.127, 4.018, 0.542],
+                [0, 0.693, 10, 1.661],
+            ]),
+            decimal=3
+        )
+        np.testing.assert_array_almost_equal(
+            softplus.deriv.deriv(test_matrix),
+            differentiate(softplus.deriv, test_matrix)
+        )
+        np.testing.assert_array_almost_equal(
+            softplus.deriv(test_matrix),
+            differentiate(softplus, test_matrix)
+        )
 
     def test_softmax(self):
-        # softmax
-        result = softmax(np.random.random((4, 5)))
+        input_matrix = np.random.random((4, 5))
+        result = softmax(input_matrix)
         self.assertEqual(result.sum(), 4)
 
-        actual = softmax(np.array([[1, 2, 3]]))
-        expected = np.array([[0.09,  0.2447, 0.6652]])
-        np.testing.assert_array_equal(np.round(actual, 4), expected)
+        np.testing.assert_array_almost_equal(
+            softmax(np.array([[1, 2, 3]])),
+            np.array([[0.09,  0.2447, 0.6652]]),
+            decimal=4
+        )
+        np.testing.assert_array_almost_equal(
+            softmax.deriv(np.array([[4, 2, 3]])),
+            np.array([
+                [[0.223, -0.06, -0.163]],
+                [[-0.06, 0.082, -0.022]],
+                [[-0.163, -0.022, 0.185]],
+            ]),
+            decimal=3
+        )