Fix error delta computation for the converge iterator method.

neupy/network/utils.py
@@ -27,14 +27,17 @@ def add_bias_column(data):
 def iter_until_converge(network, epsilon, max_epochs):
     # Trigger first iteration and store first error term
     yield network.epoch
-    error = network.last_error()
+    previous_error = error_delta = network.last_error()
 
-    while error > epsilon:
+    while error_delta > epsilon:
         network.epoch += 1
         yield network.epoch
-        error = abs(network.last_error() - error)
 
-        if network.epoch >= max_epochs and error > epsilon:
+        last_error = network.last_error()
+        error_delta = abs(last_error - previous_error)
+        previous_error = last_error
+
+        if network.epoch >= max_epochs and error_delta > epsilon:
             network.logs.log(
                 "TRAIN",
                 "Epoch #{} stopped. Network didn't converge "
@@ -42,7 +45,7 @@ def iter_until_converge(network, epsilon, max_epochs):
             )
             return
 
-    if np.isnan(error) or np.isinf(error):
+    if np.isnan(error_delta) or np.isinf(error_delta):
         network.logs.log("TRAIN", "Epoch #{} stopped. Network error value is "
                                   "invalid".format(network.epoch))
     else:

tests/associative/test_oja.py
@@ -77,7 +77,7 @@ class OjaTestCase(BaseTestCase):
         data = np.array([[1, 2, 3]]).T
         target = np.array([[1, 2, 3]]).T
 
-        ojanet.train(data, epsilon=0.1, epochs=1000)
+        ojanet.train(data, epsilon=0.01, epochs=100)
         self.assertInvalidVectorPred(ojanet, data.ravel(), target,
                                      decimal=2)
 
@@ -89,7 +89,7 @@ class OjaTestCase(BaseTestCase):
         target = np.array([[1, 2, 3]]).T
         input_vector = data.ravel()
 
-        ojanet.train(data, epsilon=0.1, epochs=1000)
+        ojanet.train(data, epsilon=0.01, epochs=100)
 
         test_vectors = create_vectors(input_vector)
 