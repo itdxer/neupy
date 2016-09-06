d7521edae713ed7b16e9a769148c2144777dbd5e
Yurii Shevchuk <mail@itdxer.com>
2016-09-06 19:20:27 +0300

    init recurrent layers modules.

---
+++ neupy/layers/recurrent.py
@@ -0,0 +1,16 @@
+from .base import BaseLayer
+
+
+__all__ = ('LSTM', 'GRU')
+
+
+class LSTM(BaseLayer):
+    """
+    Long Short Term Memory (LSTM) Layer.
+    """
+
+
+class GRU(BaseLayer):
+    """
+    Gated Recurrent Unit (GRU) Layer.
+    """
