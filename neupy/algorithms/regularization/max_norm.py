a01637dbbfa95c3016924c98882e194d093398f1
Yurii Shevchuk <mail@itdxer.com>
2017-04-05 23:14:40 +0200

    added docs to the max-norm regularizer

--- neupy/algorithms/regularization/max_norm.py
+++ neupy/algorithms/regularization/max_norm.py
@@ -7,8 +7,22 @@ from .base import WeightUpdateConfigurable
 __all__ = ('MaxNormRegularization',)
 
 
-def max_norm_clip(array, max_norm=3):
-    array_norm = T.nlinalg.norm(array, ord=None)
+def max_norm_clip(array, max_norm):
+    """
+    Clips norm of the array in case if it will exceed
+    specified limit.
+
+    Parameters
+    ----------
+    array : array-like
+        Any array-like object.
+
+    max_norm : float
+        Maximum possible norm value for the input array. Array will
+        be clipped in case if its norm will be greater than
+        specified limit.
+    """
+    array_norm = T.sqrt(T.sum(array ** 2))
     return T.switch(
         T.ge(array_norm, max_norm),
         T.mul(max_norm, array) / array_norm,
@@ -18,13 +32,46 @@ def max_norm_clip(array, max_norm=3):
 
 class MaxNormRegularization(WeightUpdateConfigurable):
     """
-    Max-norm regularization.
+    Max-norm regularization algorithm will clip norm of the
+    parameter in case if it will exceed maximum limit.
+
+    .. code-block:: python
+
+        if norm(weight) > max_norm:
+            weight = max_norm * weight / norm(weight)
+
+    .. raw:: html
+
+        <br>
+
+    Warns
+    -----
+    {WeightUpdateConfigurable.Warns}
 
     Parameters
     ----------
     max_norm : int, float
+        Any parameter that has norm greater than this value
+        will be clipped. Defaults to ``10``.
+
+    Examples
+    --------
+    >>> from neupy import algorithms
+    >>> bpnet = algorithms.GradientDescent(
+    ...     (2, 4, 1),
+    ...     step=0.1,
+    ...     max_norm=4,
+    ...     addons=[algorithms.MaxNormRegularization]
+    ... )
+
+    References
+    ----------
+    [1] N. Srivastava, G. Hinton, A. Krizhevsky, I. Sutskever,
+      R. Salakhutdinov. Dropout: A Simple Way to Prevent
+      Neural Networks from Overfitting.
+      http://jmlr.org/papers/volume15/srivastava14a/srivastava14a.pdf
     """
-    max_norm = NumberProperty(default=3, minval=0)
+    max_norm = NumberProperty(default=10, minval=0)
 
     def init_param_updates(self, layer, parameter):
         updates = super(MaxNormRegularization, self).init_param_updates(
---
+++ site/apidocs/neupy.algorithms.regularization.max_norm.rst
@@ -0,0 +1,7 @@
+neupy.algorithms.regularization.max_norm module
+===============================================
+
+.. automodule:: neupy.algorithms.regularization.max_norm
+    :members:
+    :undoc-members:
+    :show-inheritance:
