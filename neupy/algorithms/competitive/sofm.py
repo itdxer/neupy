5a7016eaae1b08aac932e3884cf61a5f70d6dc8d
Yurii Shevchuk <mail@itdxer.com>
2017-05-03 22:37:40 +0200

    added function that finds neighbours on the hexagon grid

--- neupy/algorithms/competitive/sofm.py
+++ neupy/algorithms/competitive/sofm.py
@@ -11,7 +11,8 @@ from neupy.algorithms.associative.base import BaseAssociative
 from neupy.core.docs import shared_docs
 from neupy.core.properties import (IntProperty, TypedListProperty,
                                    ChoiceProperty, NumberProperty,
-                                   ParameterProperty, BaseProperty)
+                                   ParameterProperty, BaseProperty,
+                                   Property)
 
 
 __all__ = ('SOFM',)
@@ -39,14 +40,14 @@ def gaussian_df(data, mean=0, std=1):
     return np.exp(-np.square(data - mean) / normalizer)
 
 
-def find_neighbour_distance(neurons, center):
+def find_neighbour_distance(grid, center):
     """
     Returns distance from the center into different directions
     per each dimension separately.
 
     Parameters
     ----------
-    neurons : array-like
+    grid : array-like
        Array that contains grid of n-dimensional vectors.
 
     center : tuple
@@ -57,48 +58,48 @@ def find_neighbour_distance(neurons, center):
     -------
     list of n-dimensional vectors
     """
-    if len(center) != neurons.ndim:
+    if len(center) != grid.ndim:
         raise ValueError(
             "Cannot find center, because grid of neurons has {} dimensions "
             "and center has specified coordinates for {} dimensional grid"
-            "".format(neurons.ndim, len(center)))
+            "".format(grid.ndim, len(center)))
 
     slices = []
-    for dim_length, center_coord in zip(neurons.shape, center):
+    for dim_length, center_coord in zip(grid.shape, center):
         slices.append(slice(-center_coord, dim_length - center_coord))
 
     return np.ogrid[slices]
 
 
 @shared_docs(find_neighbour_distance)
-def gaussian_neighbours(neurons, center, std=1):
+def gaussian_neighbours(grid, center, std=1):
     """
     Function returns multivariate gaussian around the center
     with specified standard deviation.
 
     Parameters
     ----------
-    {find_neighbour_distance.neurons}
+    {find_neighbour_distance.grid}
 
     {find_neighbour_distance.center}
 
     std : int, float
         Gaussian standard deviation. Defaults to ``1``.
     """
-    distances = find_neighbour_distance(neurons, center)
+    distances = find_neighbour_distance(grid, center)
     gaussian_array = sum(gaussian_df(dist, std=std) for dist in distances)
-    return gaussian_array / neurons.ndim
+    return gaussian_array / grid.ndim
 
 
 @shared_docs(find_neighbour_distance)
-def neuron_neighbours(neurons, center, radius):
+def find_neighbours_on_grid(grid, center, radius):
     """
     Function find all neuron's neighbours around specified
     center within a certain radius.
 
     Parameters
     ----------
-    {find_neighbour_distance.neurons}
+    {find_neighbour_distance.grid}
 
     {find_neighbour_distance.center}
 
@@ -109,31 +110,95 @@ def neuron_neighbours(neurons, center, radius):
     Returns
     -------
     array-like
-        Return matrix with the same dimension as ``neurons``
+        Return matrix with the same dimension as ``grid``
         where center element and it neighbours positions
         filled with value ``1`` and other as a ``0`` value.
 
     Examples
     --------
     >>> import numpy as np
-    >>> from neupy.algorithms.competitive.sofm import neuron_neighbours
+    >>> from neupy.algorithms.competitive import sofm
     >>>
-    >>> neuron_neighbours(np.zeros((3, 3)), (0, 0), 1)
+    >>> sofm.find_neighbours_on_grid(
+    ...     grid=np.zeros((3, 3)),
+    ...     center=(0, 0),
+    ...     radius=1)
+    ...
     array([[ 1.,  1.,  0.],
            [ 1.,  0.,  0.],
            [ 0.,  0.,  0.]])
     >>>
-    >>> neuron_neighbours(np.zeros((5, 5)), (2, 2), 2)
+    >>> sofm.find_neighbours_on_grid(
+    ...     grid=np.zeros((5, 5)),
+    ...     center=(2, 2),
+    ...     radius=2)
+    ...
     array([[ 0.,  0.,  1.,  0.,  0.],
            [ 0.,  1.,  1.,  1.,  0.],
            [ 1.,  1.,  1.,  1.,  1.],
            [ 0.,  1.,  1.,  1.,  0.],
            [ 0.,  0.,  1.,  0.,  0.]])
     """
-    distances = find_neighbour_distance(neurons, center)
+    distances = find_neighbour_distance(grid, center)
     mask = sum(dist ** 2 for dist in distances) <= radius ** 2
-    neurons[mask] = 1
-    return neurons
+    grid[mask] = 1
+    return grid
+
+
+def generate_neighbours_pattern(radius):
+    cache = generate_neighbours_pattern.cache
+
+    if radius in cache:
+        return cache[radius]
+
+    size = 2 * radius + 1
+    pattern = np.zeros((size, size))
+    pattern[radius, :] = 1
+
+    for i, width in enumerate(range(size - radius, size)):
+        start_point = radius - int(np.floor(width / 2))
+        neighbour_range = slice(start_point, start_point + width)
+
+        pattern[i, neighbour_range] = 1
+        # every row in the pattern is mirror symmetric
+        pattern[size - i - 1, neighbour_range] = 1
+
+    cache[radius] = pattern
+    return pattern
+
+
+generate_neighbours_pattern.cache = {}
+
+
+def find_neighbours_on_hexagon_grid(grid, center, radius):
+    center_x, center_y = center
+    pattern = generate_neighbours_pattern(radius)
+
+    # We adding zero paddings in order to be able
+    # insert full pattern into the matrix. It's
+    # useful in case if we are not able to mark all
+    # neighbours from the pattern for specific neuron.
+    # For instance, in case when center = (0, 0) we
+    # are not able to mark neurons on the left and top
+    # sides of the pattern's center.
+    grid = np.pad(grid, radius, mode='constant')
+
+    if center_x % 2 == 1:
+        # Since every even row shifted to the right
+        # compare to the odd rows then odd rows shifted
+        # to the left compare to the even rows.
+        # It means that our pattern has to be reversed
+        # for the even rows.
+        pattern = pattern[:, ::-1]
+
+    pattern_size = 2 * radius + 1
+    x_range = slice(center_x, center_x + pattern_size)
+    y_range = slice(center_y, center_y + pattern_size)
+
+    grid[x_range, y_range] = pattern
+
+    # removing paddings
+    return grid[radius:-radius, radius:-radius]
 
 
 def neg_euclid_distance(input_data, weight):
@@ -446,6 +511,7 @@ class SOFM(Kohonen):
         })
 
     features_grid = TypedListProperty(allow_none=True, default=None)
+    use_hexagon_grid = Property(expected_type=bool, default=False)
     distance = ChoiceProperty(
         default='euclid',
         choices={
@@ -526,14 +592,14 @@ class SOFM(Kohonen):
             std = decay_function(std, self.last_epoch,
                                  self.reduce_std_after)
 
-        output_with_neightbours = neuron_neighbours(
-            neurons=np.reshape(layer_output, self.features_grid),
+        output_with_neightbours = find_neighbours_on_grid(
+            grid=np.reshape(layer_output, self.features_grid),
             center=winner_neuron_coords,
             radius=learning_radius)
 
         step_scaler = gaussian_neighbours(
-            np.reshape(layer_output, self.features_grid),
-            winner_neuron_coords,
+            grid=np.reshape(layer_output, self.features_grid),
+            center=winner_neuron_coords,
             std=std)
 
         step_scaler = step_scaler.reshape(self.n_outputs)
