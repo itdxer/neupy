c8c96ea0922aa416a58d3be0bc14e064e267873f
Yurii Shevchuk <mail@itdxer.com>
2017-08-01 14:18:31 +0200

    changed the way mixture of experts used

--- tests/algorithms/ensemble/test_mixtures_of_experts.py
+++ tests/algorithms/ensemble/test_mixtures_of_experts.py
@@ -1,8 +1,8 @@
 import numpy as np
 from sklearn import datasets, preprocessing, model_selection
+
 from neupy import algorithms, layers
-from neupy.utils import asfloat
-from neupy.estimators import rmsle
+from neupy.estimators import categorical_crossentropy
 
 from base import BaseTestCase
 
@@ -16,194 +16,141 @@ class MixtureOfExpertsTestCase(BaseTestCase):
                 step=0.2,
                 verbose=False
             ),
-            algorithms.GradientDescent(
-                (1, 20, 1),
-                step=0.2,
-                verbose=False
+            layers.join(
+                layers.Input(1),
+                layers.Sigmoid(20),
+                layers.Sigmoid(1)
             ),
         ]
 
-    def test_mixture_of_experts_init_networks_exceptions(self):
-        networks = self.networks
+    def test_mixture_of_experts_problem_with_specific_network(self):
+        with self.assertRaisesRegexp(ValueError, "specified as a list"):
+            algorithms.mixture_of_experts(*self.networks)
 
-        with self.assertRaises(ValueError):
-            # Invalid network (not GradientDescent)
-            algorithms.MixtureOfExperts(
-                networks=networks + [
-                    algorithms.GRNN(verbose=False)
-                ],
-                gating_network=algorithms.GradientDescent(
-                    layers.Input(1) > layers.Sigmoid(3),
-                    verbose=False,
-                )
-            )
+        with self.assertRaisesRegexp(ValueError, "has more than one input"):
+            algorithms.mixture_of_experts(
+                networks=self.networks + [
+                    [layers.Input(1), layers.Input(1)] > layers.Softmax(1),
+                ])
 
-        with self.assertRaises(ValueError):
-            # Invalid number of outputs in third network
-            algorithms.MixtureOfExperts(
-                networks=networks + [
-                    algorithms.GradientDescent(
-                        (1, 20, 2),
-                        step=0.2,
-                        verbose=False
-                    )
-                ],
-                gating_network=algorithms.GradientDescent(
-                    layers.Input(1) > layers.Sigmoid(3),
-                    verbose=False,
-                )
-            )
+        with self.assertRaisesRegexp(ValueError, "has more than one output"):
+            algorithms.mixture_of_experts(
+                networks=self.networks + [
+                    layers.Input(1) > [layers.Softmax(1), layers.Softmax(1)],
+                ])
 
-        with self.assertRaises(ValueError):
-            # Invalid network error function
-            algorithms.MixtureOfExperts(
-                networks=networks + [
-                    algorithms.GradientDescent(
-                        (1, 20, 1),
-                        step=0.2,
-                        error='rmsle',
-                        verbose=False,
-                    )
-                ],
-                gating_network=algorithms.GradientDescent(
-                    layers.Input(1) > layers.Sigmoid(3),
-                    verbose=False,
-                ),
-            )
+        with self.assertRaisesRegexp(ValueError, "should receive vector"):
+            algorithms.mixture_of_experts(
+                networks=self.networks + [layers.Input((1, 1, 1))])
+
+    def test_mixture_of_experts_problem_with_incompatible_networks(self):
+        with self.assertRaisesRegexp(ValueError, "different input shapes"):
+            algorithms.mixture_of_experts(
+                networks=self.networks + [layers.Input(10)])
+
+        with self.assertRaisesRegexp(ValueError, "different output shapes"):
+            algorithms.mixture_of_experts(
+                networks=self.networks + [
+                    layers.Input(1) > layers.Relu(10)
+                ])
 
     def test_mixture_of_experts_init_gating_network_exceptions(self):
-        networks = self.networks
+        with self.assertRaisesRegexp(ValueError, "Invalid type"):
+            algorithms.mixture_of_experts(
+                networks=self.networks,
+                gating_layer=(layers.Input(1) > layers.Softmax(2)))
 
-        with self.assertRaises(ValueError):
-            # Invalid gating error function
-            algorithms.MixtureOfExperts(
-                networks=networks,
-                gating_network=algorithms.GradientDescent(
-                    layers.Input(1) > layers.Softmax(2),
-                    error='rmsle',
-                    verbose=False
-                ),
-            )
+        with self.assertRaisesRegexp(ValueError, "invalid number of outputs"):
+            algorithms.mixture_of_experts(
+                networks=self.networks,
+                gating_layer=layers.Softmax(10))
 
-        with self.assertRaises(ValueError):
-            # Invalid gating network algorithm
-            algorithms.MixtureOfExperts(
-                networks=networks,
-                gating_network=algorithms.PNN(),
-            )
+    def test_mixture_of_experts_multi_class_classification(self):
+        import copy
+        insize, outsize = (10, 3)
+        n_epochs = 10
 
-        with self.assertRaises(ValueError):
-            # Invalid gating network output layer
-            algorithms.MixtureOfExperts(
-                networks=networks,
-                gating_network=algorithms.GradientDescent(
-                    layers.Input(1) > layers.Sigmoid(2),
-                    verbose=False,
-                )
-            )
+        default_configs = dict(
+            step=0.1,
+            batch_size=10,
+            error='categorical_crossentropy',
+            verbose=False)
 
-        with self.assertRaises(ValueError):
-            # Invalid gating network output layer size
-            algorithms.MixtureOfExperts(
-                networks=networks,
-                gating_network=algorithms.GradientDescent(
-                    layers.Input(1) > layers.Softmax(1),
-                    verbose=False,
-                )
-            )
+        architecture = layers.join(
+            layers.Input(insize),
+            layers.Relu(20),
+            layers.Softmax(outsize))
 
-    def test_mixture_of_experts_training_exceptions(self):
-        moe = algorithms.MixtureOfExperts(
-            # Invalid gating network output layer
-            networks=self.networks,
-            gating_network=algorithms.GradientDescent(
-                layers.Input(1) > layers.Softmax(2),
-                verbose=False
-            ),
-        )
-        with self.assertRaises(ValueError):
-            # Wrong number of train input features
-            moe.train(np.array([[1, 2]]), np.array([[0]]))
-
-        with self.assertRaises(ValueError):
-            # Wrong number of train output features
-            moe.train(np.array([[1]]), np.array([[0, 0]]))
-
-    def test_mixture_of_experts(self):
-        dataset = datasets.load_diabetes()
-        data, target = asfloat(dataset.data), asfloat(dataset.target)
-        insize, outsize = data.shape[1], 1
+        data, target = datasets.make_classification(
+            n_samples=200,
+            n_features=insize,
+            n_classes=outsize,
+            n_clusters_per_class=2,
+            n_informative=5)
 
         input_scaler = preprocessing.MinMaxScaler((-1, 1))
-        output_scaler = preprocessing.MinMaxScaler()
+        one_hot = preprocessing.OneHotEncoder()
+
+        target = target.reshape((-1, 1))
+        encoded_target = one_hot.fit_transform(target)
         x_train, x_test, y_train, y_test = model_selection.train_test_split(
             input_scaler.fit_transform(data),
-            output_scaler.fit_transform(target.reshape(-1, 1)),
-            train_size=0.8
-        )
-
-        n_epochs = 10
-        scaled_y_test = output_scaler.inverse_transform(y_test)
-        scaled_y_test = scaled_y_test.reshape((y_test.size, 1))
+            np.asarray(encoded_target.todense()),
+            train_size=0.8)
 
         # -------------- Train single GradientDescent -------------- #
 
-        bpnet = algorithms.GradientDescent(
-            (insize, 20, outsize),
-            step=0.1,
-            verbose=False
+        bpnet = algorithms.Momentum(
+            copy.deepcopy(architecture),
+            **default_configs
         )
+
         bpnet.train(x_train, y_train, epochs=n_epochs)
         network_output = bpnet.predict(x_test)
-        network_error = rmsle(output_scaler.inverse_transform(network_output),
-                              scaled_y_test)
+
+        network_error = categorical_crossentropy(y_test, network_output)
 
         # -------------- Train ensemlbe -------------- #
 
-        moe = algorithms.MixtureOfExperts(
-            networks=[
-                algorithms.Momentum(
-                    (insize, 20, outsize),
-                    step=0.1,
-                    batch_size=1,
-                    verbose=False
-                ),
-                algorithms.Momentum(
-                    (insize, 20, outsize),
-                    step=0.1,
-                    batch_size=1,
-                    verbose=False
-                ),
-            ],
-            gating_network=algorithms.Momentum(
-                layers.Input(insize) > layers.Softmax(2),
-                step=0.1,
-                verbose=False
-            )
+        moe = algorithms.Momentum(
+            algorithms.mixture_of_experts([
+                copy.deepcopy(architecture),
+                copy.deepcopy(architecture),
+                copy.deepcopy(architecture),
+            ]),
+            **default_configs
         )
         moe.train(x_train, y_train, epochs=n_epochs)
         ensemble_output = moe.predict(x_test)
 
-        ensemlbe_error = rmsle(
-            output_scaler.inverse_transform(ensemble_output),
-            scaled_y_test
-        )
-
+        ensemlbe_error = categorical_crossentropy(y_test, ensemble_output)
         self.assertGreater(network_error, ensemlbe_error)
 
-    def test_mixture_of_experts_repr(self):
-        moe = algorithms.MixtureOfExperts(
-            networks=[
-                algorithms.Momentum((3, 2, 1)),
-                algorithms.GradientDescent((3, 2, 1)),
-            ],
-            gating_network=algorithms.Adadelta(
-                layers.Input(3) > layers.Softmax(2),
-            )
-        )
-        moe_repr = str(moe)
+    def test_mixture_of_experts_architecture(self):
+        network = algorithms.mixture_of_experts([
+            layers.join(
+                layers.Input(10),
+                layers.Relu(5),
+            ),
+            layers.join(
+                layers.Input(10),
+                layers.Relu(20),
+                layers.Relu(5),
+            ),
+            layers.join(
+                layers.Input(10),
+                layers.Relu(30),
+                layers.Relu(40),
+                layers.Relu(5),
+            ),
+        ])
 
-        self.assertIn('MixtureOfExperts', moe_repr)
-        self.assertIn('Momentum', moe_repr)
-        self.assertIn('GradientDescent', moe_repr)
-        self.assertIn('Adadelta', moe_repr)
+        self.assertEqual(len(network), 12)
+        self.assertEqual(network.input_shape, (10,))
+        self.assertEqual(network.output_shape, (5,))
+
+        predict = network.compile()
+        random_input = np.random.random((3, 10))
+        prediction = predict(random_input)
+
+        self.assertEqual(prediction.shape, (3, 5))
