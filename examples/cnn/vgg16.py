587cc1d9fd9a57560a7daa304aa248e0b07fbee9
Yurii Shevchuk <mail@itdxer.com>
2017-08-06 17:57:20 +0200

    move vgg16, vgg19, alexnet and squeezenet into new architectures module

--- examples/cnn/vgg16.py
+++ examples/cnn/vgg16.py
@@ -1,7 +1,7 @@
 import os
 
 import theano
-from neupy import layers, storage
+from neupy import layers, storage, architectures
 
 from imagenet_tools import (CURRENT_DIR, FILES_DIR, load_image,
                             print_top_n, download_file)
@@ -10,38 +10,7 @@ from imagenet_tools import (CURRENT_DIR, FILES_DIR, load_image,
 theano.config.floatX = 'float32'
 VGG16_WEIGHTS_FILE = os.path.join(FILES_DIR, 'vgg16.pickle')
 
-vgg16 = layers.join(
-    layers.Input((3, 224, 224)),
-
-    layers.Convolution((64, 3, 3), padding=1, name='conv1_1') > layers.Relu(),
-    layers.Convolution((64, 3, 3), padding=1, name='conv1_2') > layers.Relu(),
-    layers.MaxPooling((2, 2)),
-
-    layers.Convolution((128, 3, 3), padding=1, name='conv2_1') > layers.Relu(),
-    layers.Convolution((128, 3, 3), padding=1, name='conv2_2') > layers.Relu(),
-    layers.MaxPooling((2, 2)),
-
-    layers.Convolution((256, 3, 3), padding=1, name='conv3_1') > layers.Relu(),
-    layers.Convolution((256, 3, 3), padding=1, name='conv3_2') > layers.Relu(),
-    layers.Convolution((256, 3, 3), padding=1, name='conv3_3') > layers.Relu(),
-    layers.MaxPooling((2, 2)),
-
-    layers.Convolution((512, 3, 3), padding=1, name='conv4_1') > layers.Relu(),
-    layers.Convolution((512, 3, 3), padding=1, name='conv4_2') > layers.Relu(),
-    layers.Convolution((512, 3, 3), padding=1, name='conv4_3') > layers.Relu(),
-    layers.MaxPooling((2, 2)),
-
-    layers.Convolution((512, 3, 3), padding=1, name='conv5_1') > layers.Relu(),
-    layers.Convolution((512, 3, 3), padding=1, name='conv5_2') > layers.Relu(),
-    layers.Convolution((512, 3, 3), padding=1, name='conv5_3') > layers.Relu(),
-    layers.MaxPooling((2, 2)),
-
-    layers.Reshape(),
-
-    layers.Relu(4096, name='dense_1') > layers.Dropout(0.5),
-    layers.Relu(4096, name='dense_2') > layers.Dropout(0.5),
-    layers.Softmax(1000, name='dense_3'),
-)
+vgg16 = architectures.vgg16()
 
 if not os.path.exists(VGG16_WEIGHTS_FILE):
     download_file(
---
+++ neupy/architectures/vgg16.py
@@ -0,0 +1,39 @@
+from neupy import layers
+
+
+__all__ = ('vgg16',)
+
+
+def vgg16():
+    return layers.join(
+        layers.Input((3, 224, 224)),
+
+        layers.Convolution((64, 3, 3), padding=1, name='conv1_1') > layers.Relu(),
+        layers.Convolution((64, 3, 3), padding=1, name='conv1_2') > layers.Relu(),
+        layers.MaxPooling((2, 2)),
+
+        layers.Convolution((128, 3, 3), padding=1, name='conv2_1') > layers.Relu(),
+        layers.Convolution((128, 3, 3), padding=1, name='conv2_2') > layers.Relu(),
+        layers.MaxPooling((2, 2)),
+
+        layers.Convolution((256, 3, 3), padding=1, name='conv3_1') > layers.Relu(),
+        layers.Convolution((256, 3, 3), padding=1, name='conv3_2') > layers.Relu(),
+        layers.Convolution((256, 3, 3), padding=1, name='conv3_3') > layers.Relu(),
+        layers.MaxPooling((2, 2)),
+
+        layers.Convolution((512, 3, 3), padding=1, name='conv4_1') > layers.Relu(),
+        layers.Convolution((512, 3, 3), padding=1, name='conv4_2') > layers.Relu(),
+        layers.Convolution((512, 3, 3), padding=1, name='conv4_3') > layers.Relu(),
+        layers.MaxPooling((2, 2)),
+
+        layers.Convolution((512, 3, 3), padding=1, name='conv5_1') > layers.Relu(),
+        layers.Convolution((512, 3, 3), padding=1, name='conv5_2') > layers.Relu(),
+        layers.Convolution((512, 3, 3), padding=1, name='conv5_3') > layers.Relu(),
+        layers.MaxPooling((2, 2)),
+
+        layers.Reshape(),
+
+        layers.Relu(4096, name='dense_1') > layers.Dropout(0.5),
+        layers.Relu(4096, name='dense_2') > layers.Dropout(0.5),
+        layers.Softmax(1000, name='dense_3'),
+    )
