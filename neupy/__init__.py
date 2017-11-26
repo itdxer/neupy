1bb483213f5f82d6e0ebea50d3b098094b757146
Yurii Shevchuk <mail@itdxer.com>
2017-11-26 21:07:12 +0100

    simplified summary info functinality and disabled theano warning

--- neupy/__init__.py
+++ neupy/__init__.py
@@ -1,6 +1,10 @@
 """
 NeuPy is a Python library for Artificial Neural Networks and Deep Learning.
 """
+import theano
 
 
 __version__ = '0.6.1'
+
+# Disable annoying warning from Theano
+theano.config.warn.round = False
