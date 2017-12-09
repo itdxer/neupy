4e4e846da4cd0f1048b63df4440a107c76c3aeb3
Yurii Shevchuk <mail@itdxer.com>
2017-12-09 18:58:44 +0100

    fixed bug in sofm weight init comparison example

--- examples/competitive/sofm_compare_weight_init.py
+++ examples/competitive/sofm_compare_weight_init.py
@@ -56,7 +56,9 @@ if __name__ == '__main__':
             step=0.3,
             reduce_step_after=5,
         )
-        sofm.init_weights(data)
+
+        if not sofm.initialized:
+            sofm.init_weights(data)
 
         plt.subplot(n_rows, n_columns, index)
 
