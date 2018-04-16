9ded4f1303ec200bcbe4085e983cafc2485ca976
Yurii Shevchuk <mail@itdxer.com>
2018-04-16 22:27:34 +0200

    updated adam test

--- tests/algorithms/gd/test_adam.py
+++ tests/algorithms/gd/test_adam.py
@@ -6,15 +6,14 @@ from base import BaseTestCase
 
 class AdamTestCase(BaseTestCase):
     def test_simple_adam(self):
-        x_train, _, y_train, _ = simple_classification()
+        x_train, x_test, y_train, y_test = simple_classification()
         mnet = algorithms.Adam(
             (10, 20, 1),
-            step=1.,
-            batch_size='full',
+            step=10.,
             verbose=False,
-            epsilon=1e-8,
+            epsilon=1e-7,
             beta1=0.9,
             beta2=0.999,
         )
-        mnet.train(x_train, y_train, epochs=100)
-        self.assertAlmostEqual(0.06, mnet.errors.last(), places=2)
+        mnet.train(x_train, y_train, x_test, y_test, epochs=100)
+        self.assertAlmostEqual(0.08, mnet.errors.last(), places=2)
