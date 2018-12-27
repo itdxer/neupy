f8f45fa9ec8bad7745adfd03cd2f6d2330bd2ed0
Yurii Shevchuk <mail@itdxer.com>
2018-12-27 16:34:11 +0100

    added more tests for utils.iters

--- tests/algorithms/gd/test_gd.py
+++ tests/algorithms/gd/test_gd.py
@@ -1,37 +1,13 @@
 from functools import partial
 
-from neupy import algorithms, layers, utils
+from neupy import algorithms, layers
 
-from helpers import compare_networks
 from base import BaseTestCase
 from helpers import simple_classification
 
 
 class GradientDescentTestCase(BaseTestCase):
-    def test_network_attrs(self):
-        network = algorithms.GradientDescent(
-            [
-                layers.Input(2),
-                layers.Sigmoid(2),
-                layers.Sigmoid(1),
-            ],
-            verbose=False,
-        )
-        network.step = 0.1
-        network.error = 'mse'
-        network.shuffle_data = True
-
-        with self.assertRaises(TypeError):
-            network.step = '33'
-
-        with self.assertRaises(ValueError):
-            network.error = 'not a function'
-
-        with self.assertRaises(TypeError):
-            network.shuffle_data = 1
-
     def test_gd(self):
-        utils.reproducible()
         x_train, _, y_train, _ = simple_classification()
 
         network = algorithms.GradientDescent(
@@ -42,30 +18,14 @@ class GradientDescentTestCase(BaseTestCase):
         network.train(x_train, y_train, epochs=100)
         self.assertLess(network.training_errors[-1], 0.05)
 
-    def test_minibatch_gd(self):
-        x_train, _, y_train, _ = simple_classification()
-        compare_networks(
-           # Test classes
-           partial(algorithms.GradientDescent, verbose=False),
-           partial(algorithms.GradientDescent,
-                   batch_size=1, verbose=False),
-           # Test data
-           (x_train, y_train),
-           # Network configurations
-           connection=(layers.Input(10) > layers.Tanh(20) > layers.Tanh(1)),
-           step=0.02,
-           shuffle_data=True,
-           verbose=False,
-           # Test configurations
-           epochs=40,
-           show_comparison_plot=False
-        )
-
     def test_gd_get_params_method(self):
         network = algorithms.GradientDescent(
             layers.Input(2) > layers.Sigmoid(3) > layers.Sigmoid(1))
 
-        self.assertIn('connection', network.get_params(with_connection=True))
+        self.assertIn(
+            'connection',
+            network.get_params(with_connection=True),
+        )
         self.assertNotIn(
             'connection',
             network.get_params(with_connection=False),
@@ -88,8 +48,6 @@ class GradientDescentTestCase(BaseTestCase):
             epochs=4000,
         )
 
-
-class GDAdditionalFunctionsTestCase(BaseTestCase):
     def test_small_network_representation(self):
         network = algorithms.GradientDescent(
             layers.Input(2) > layers.Sigmoid(3) > layers.Sigmoid(1))
