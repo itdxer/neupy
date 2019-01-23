bd27ae7ffc2be6bc5906e347f1ecaed3f8749c1c
Yurii Shevchuk <mail@itdxer.com>
2019-01-23 12:07:58 +0100

    added plots for errors

--- neupy/algorithms/plots.py
+++ neupy/algorithms/plots.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 def load_pandas_module():
     if not pkgutil.find_loader('pandas'):
         raise ImportError(
-            "The `pandas` library wasn't installed. Try to "
+            "The `pandas` library is not installed. Try to "
             "install it with pip: \n    pip install pandas")
 
     return importlib.import_module('pandas')
---
+++ tests/algorithms/test_plots.py
@@ -0,0 +1,84 @@
+import warnings
+
+import mock
+import pytest
+import matplotlib.pyplot as plt
+
+from neupy import algorithms, layers
+from neupy.algorithms.plots import load_pandas_module
+
+from helpers import simple_classification
+from base import BaseTestCase
+
+
+class PlotsTestCase(BaseTestCase):
+    def setUp(self):
+        super(PlotsTestCase, self).setUp()
+        self.network = layers.join(
+            layers.Input(10),
+            layers.Sigmoid(20),
+            layers.Sigmoid(1),
+        )
+
+    def test_failed_pandas_import(self):
+        with mock.patch('pkgutil.find_loader') as mock_find_loader:
+            mock_find_loader.return_value = None
+
+            message = "The `pandas` library is not installed"
+            with self.assertRaisesRegexp(ImportError, message):
+                load_pandas_module()
+
+    def test_not_tarining_data_to_plot(self):
+        optimizer = algorithms.Adadelta(self.network)
+
+        with warnings.catch_warnings(record=True) as warns:
+            optimizer.plot_errors()
+            self.assertEqual(1, len(warns))
+            self.assertEqual(
+                str(warns[-1].message),
+                "There is no data to plot")
+
+    @pytest.mark.mpl_image_compare
+    def test_plot_errors_no_batch(self):
+        x_train, x_test, y_train, y_test = simple_classification()
+
+        optimizer = algorithms.Adadelta(self.network, batch_size=None)
+        optimizer.train(x_train, y_train, x_test, y_test, epochs=100)
+        optimizer.plot_errors(show=False)
+
+        return plt.gcf()
+
+    @pytest.mark.mpl_image_compare
+    def test_plot_errors_batch(self):
+        x_train, x_test, y_train, y_test = simple_classification()
+
+        optimizer = algorithms.Adadelta(
+            self.network,
+            shuffle_data=True,
+            batch_size=10,
+        )
+        optimizer.train(x_train, y_train, x_test, y_test, epochs=100)
+        optimizer.plot_errors(show=False)
+
+        return plt.gcf()
+
+    @pytest.mark.mpl_image_compare
+    def test_plot_errors_show_triggered_automatically(self):
+        x_train, x_test, y_train, y_test = simple_classification()
+
+        optimizer = algorithms.Adadelta(
+            self.network,
+            shuffle_data=True,
+            batch_size=10,
+        )
+        optimizer.train(x_train, y_train, epochs=100)
+        events = []
+
+        def mocked_show(*args, **kwargs):
+            events.append('show')
+
+        with mock.patch('matplotlib.pyplot.show', side_effect=mocked_show):
+            optimizer.plot_errors(show=True)
+            self.assertSequenceEqual(events, ['show'])
+
+        return plt.gcf()
